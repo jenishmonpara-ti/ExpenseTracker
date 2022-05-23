4bd5b42bffd9fdf8cbb901509bef6c199b8683bb
jenishmonpara <54908938+jenishmonpara@users.noreply.github.com>
2022-05-23 20:39:41 +0530

    Extracts from fast api blog in main.py. Now main.py will be modified for the project

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # run "uvicorn main:app --reload"
 # used --reload => updating application code, the server will reload automatically.
 # type "http://127.0.0.1:8000/getmethat" to fetch resource that root() has to offer
-# http://127.0.0.1:8000/docs provides automatic interactive API documentation
+# http://127.0.0.1:8000/docs or /redoc provides automatic interactive API documentation
 @app.get("/getmethat")
 async def root():
     return {"message": "Hello World"}
@@ -15,3 +15,23 @@ async def root():
 @app.get("/getmethis")
 async def root(x : int):
     return x
+
+# "http://127.0.0.1:8000/getme/{which}" sample
+@app.get("/getme/{which}/{two}")
+async def root(which : int, two : bool):
+    return which
+
+
+from pydantic import BaseModel
+from typing import Optional
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
